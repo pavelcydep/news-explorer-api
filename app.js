bf2bfb96126736405e29d616d62f119183b32235
pavel <pavel/ps2014@yandex.ru>
2021-03-05 21:55:28 +0300

    Добавил

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false,
   useUnifiedTopology: true,
 });
-app.use('*',cors(corsOptions))
+app.use('*',cors(option))
 app.option('*',cors());
 
 app.use(requestLogger);
