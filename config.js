03279cde400bbf5177f7a5bd39e5aef0ec1f03d6
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-07 11:09:32 +0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/mestodb' ;
-const JWT_SECRET = (process.env.NODE_ENV === 'production') ? process.env.JWT_SECRET : 'dev-key';
-const PORT = process.env.PORT || 3000;
-module.exports = {
-  MONGODB,
-  JWT_SECRET,
-  PORT
-};
