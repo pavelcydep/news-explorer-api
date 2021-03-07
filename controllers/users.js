870e1d9d5456f3a69d85a902de9eec7537c4c96b
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-07 10:46:05 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,36 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const CustomError = require('../errors/customError');
 const User = require('../models/user');
-
-let JWT_SECRET;
-
-if (process.env.NODE_ENV !== 'production') {
-  JWT_SECRET = process.env.JWT_SECRET ? process.env.JWT_SECRET : 'dev-key';
-} else {
-  JWT_SECRET = process.env.JWT_SECRET;
-}
-
-module.exports.getUserById = (req, res, next) => {
-  User.findById(req.user._id)
-    .orFail(new CustomError(404, 'Данный пользователь не найден'))
-    .then((user) => res.status(200).send(user))
-    .catch(next);
-};
+const { JWT_SECRET } = require('../config');
 
 module.exports.createUser = (req, res, next) => {
-  const { password, email, name } = req.body;
-  bcrypt.hash(password, 10).then((hashPassword) => {
-    User.create({ password: hashPassword, email, name })
-      .then((user) => res.status(200).send({ _id: user._id }))
-      .catch((err) => {
-        if (err.code === 11000) {
-          next(new CustomError(409, err.message));
-        } else {
-          next(new CustomError(400, err.message));
-        }
-      });
-  });
+  const { name, email } = req.body;
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => User.create({ name, email, password: hash }))
+    .then((user) => res.status(201).send({
+      _id: user._id,
+      name: user.name,
+      email: user.email
+    }))
+    .catch(next);
 };
 
 module.exports.login = (req, res, next) => {
-  const { password, email } = req.body;
+  const { email, password } = req.body;
   User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
-      res.send({ token });
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET);
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true
+      });
+      res.send({ message: 'Успешная авторизация' });
     })
     .catch(next);
 };
+
+module.exports.userMe = (req, res, next) => User.findById(req.user._id)
+  .orFail()
+  .then((users) => res.send({ users }))
+  .catch(next);
