1715380080bbb78911b801f5617ef8b35be2c317
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-09 10:40:58 +0300

    Update routerIndex.js

--- routes/routerIndex.js
+++ routes/routerIndex.js
@@ -1,29 +1,5 @@
 const router = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
-
-const auth = require('../middlewares/auth');
-
-const articles = require('./articles');
-const users = require('./users');
-const {
-  createUser,
-  login,
-} = require('../controllers/users');
-
-router.post('/signin',  login);
-
-router.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().min(3),
-    password: Joi.string().required().min(3),
-    name: Joi.string().required().min(2),
-  }),
-}), createUser);
-
-router.use('/articles',  auth, articles);
-
-router.use('/users', users);
-router.use('/users/me', users);
+router.use('/', require('../routes/articles'));
+router.use('/', require('../routes/users'));
 
 module.exports = router;
-
