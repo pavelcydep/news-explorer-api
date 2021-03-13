660e6242daeb7b96ca7dbed541a0dbea611d1baa
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-13 22:29:35 +0300

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -8,14 +8,14 @@ routerArticles.get('/', findArticles);
 
 routerArticles.post('/', celebrate({
   body: Joi.object().keys({
-    keyword: Joi.string().required().min(2),
-    title: Joi.string().required().min(2),
-    text: Joi.string().required().min(2),
-    date: Joi.string().required().min(2),
-    source: Joi.string().required().min(2),
-    link: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+#?$/),
-    image: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+#?$/)
-  }),
+    keyword: Joi.string().required(),
+    title: Joi.string().required(),
+    text: Joi.string().required(),
+    date: Joi.string().required(),
+    source: Joi.string().required(),
+    link: Joi.string().required().pattern(/^(?! )http(s)?:\/\/(www\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z0-9])[.-]?){1,}([a-zA-Z0-9])\.([a-zA-Z]{2,6}))(?::\d{2,5})?(?:[\\/?#]\S*)?/),
+    image: Joi.string().required().pattern(/^(?! )http(s)?:\/\/(www\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z0-9])[.-]?){1,}([a-zA-Z0-9])\.([a-zA-Z]{2,6}))(?::\d{2,5})?(?:[\\/?#]\S*)?/)
+  })
 }), createArticles);
 
 routerArticles.delete('/:id', celebrate({
