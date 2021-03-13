45c3138a280aa8336fd3d59e952b51b448155d1f
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-13 22:22:12 +0300

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -6,10 +6,23 @@ const {
 
 routerArticles.get('/', findArticles);
 
-routerArticles.post('/', createArticles);
+routerArticles.post('/', celebrate({
+  body: Joi.object().keys({
+    keyword: Joi.string().required().min(2),
+    title: Joi.string().required().min(2),
+    text: Joi.string().required().min(2),
+    date: Joi.string().required().min(2),
+    source: Joi.string().required().min(2),
+    link: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+#?$/),
+    image: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+#?$/),
+  }),
+}), createArticles);
 
-routerArticles.delete('/:id', deleteArticles);
-
+routerArticles.delete('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().hex().required().length(24),
+  }),
+}), deleteArticles);
 
 
 module.exports = routerArticles;
