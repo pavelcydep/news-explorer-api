6c492216b21115afcb02e361efacf7c21692c082
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-14 11:30:21 +0300

    Update articles.js

--- controllers/articles.js
+++ controllers/articles.js
@@ -9,40 +9,49 @@ module.exports.findArticles = (req, res, next) => {
 
 module.exports.createArticles = (req, res, next) => {
   const {
-    keyword, title, text, date, source, link, image,
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image
   } = req.body;
   Article.create({
-    keyword, title, text, date, source, link, image, owner: req.user._id,
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+    owner: req.user._id
   })
-    .then((card) => {
-      Article.findById(card._id).populate(['owner'])
-        .orFail(new CustomError(404, 'Данного id нет в базе'))
-        .then((createArticles) => {
-          res.status(200).send(createArticles);
-        });
-    }).catch((err) => next(new CustomError(400, err.message)));
+    .then((article) => res.send({
+      id: article._id,
+      keyword: article.keyword,
+      title: article.title,
+      text: article.text,
+      date: article.date,
+      source: article.source,
+      link: article.link,
+      image: article.image
+    }))
+    .catch(next);
 };
+
 module.exports.deleteArticles = (req, res, next) => {
-  Article.findById(req.params.id).populate(['owner'])
+  Article.findById(req.params.id)
+    .select('+owner')
     .orFail(new CustomError(404, 'Данного id нет в базе'))
     .then((article) => {
-      if (article.owner._id.toString() === req.user._id.toString()) {
-        article.deleteOne().then((deletedArticle) => {
-          res.status(200).send({
-            _id: deletedArticle._id,
-            keyword: deletedArticle.keyword,
-            title: deletedArticle.title,
-            text: deletedArticle.text,
-            date: deletedArticle.date,
-            source: deletedArticle.source,
-            link: deletedArticle.link,
-            image: deletedArticle.image,
-          });
-        });
+      if (article.owner.toString() === req.user._id) {
+        Article.deleteOne({ _id: req.params.id })
+          .then((articleDel) => res.send(articleDel))
+          .catch(next);
       } else {
-        throw new CustomError(403, 'У вас нет прав на это действие');
+       throw new CustomError(403, 'У вас нет прав на это действие');
       }
     })
     .catch(next);
 };
-
