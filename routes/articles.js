af7a5dd9badb9be97e9b60aafa4aa684ccd9675f
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-14 12:21:19 +0300

    Update articles.js

--- routes/articles.js
+++ routes/articles.js
@@ -6,22 +6,8 @@ const {
 
 routerArticles.get('/', findArticles);
 
-routerArticles.post('/', celebrate({
-  body: Joi.object().keys({
-    keyword: Joi.string().required().min(2),
-    title: Joi.string().required().min(2),
-    text: Joi.string().required().min(2),
-    date: Joi.string().required().min(2),
-    source: Joi.string().required().min(2),
-   link: Joi.string().required().pattern(/^(?! )http(s)?:\/\/(www\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z0-9])[.-]?){1,}([a-zA-Z0-9])\.([a-zA-Z]{2,6}))(?::\d{2,5})?(?:[\\/?#]\S*)?/),
-    image: Joi.string().required().pattern(/^(?! )http(s)?:\/\/(www\.)?((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z0-9])[.-]?){1,}([a-zA-Z0-9])\.([a-zA-Z]{2,6}))(?::\d{2,5})?(?:[\\/?#]\S*)?/)
-  })
-}), createArticles);
+routerArticles.post('/',  createArticles);
 
-routerArticles.delete('/:id', celebrate({
-  params: Joi.object().keys({
-    id: Joi.string().hex().required().length(24),
-  }),
-}), deleteArticles);
+routerArticles.delete('/:id', deleteArticles);
 
 module.exports = routerArticles;
