983e1de592d87af8037e961ed997cb2132c8129c
pavelcydep <66935762+pavelcydep@users.noreply.github.com>
2021-03-15 18:28:13 +0300

    Update articles.js

--- controllers/articles.js
+++ controllers/articles.js
@@ -42,7 +42,7 @@ module.exports.createArticles = (req, res, next) => {
 
 module.exports.deleteArticles = (req, res, next) => {
   Article.findById(req.params.id)
-    .populate(['owner'])
+     .select('+owner')
     .orFail(new CustomError(404, 'Данного id нет в базе'))
     .then((article) => {
       if (article.owner.toString() === req.user._id) {
